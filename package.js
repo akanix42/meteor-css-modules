c804539832f4deae039f69853f519b434f5a1fc1
Nathan Reid <nathan-code@nathantreid.com>
2015-11-04 08:08:54 -0600

    Added reference to git repository

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 	// Brief, one-line summary of the package.
 	summary: 'CSS modules implementation. CSS for components!',
 	// URL to the Git repository containing the source code for this package.
-	git: '',
+	git: 'https://github.com/nathantreid/meteor-css-modules.git',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
