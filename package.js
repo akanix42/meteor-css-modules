3896c728fc41f98b8dbaf9b422ccb0055a8c50f8
Nathan Reid <nathan-code@nathantreid.com>
2016-02-02 20:09:11 -0600

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'nathantreid:css-modules',
-	version: '0.6.0',
+	version: '0.6.1',
 	// Brief, one-line summary of the package.
 	summary: 'CSS modules implementation. CSS for components!',
 	// URL to the Git repository containing the source code for this package.
