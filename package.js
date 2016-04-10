055f9e6ec28591cf8ed501d5a9e490f8cbb3f107
Nathan Reid <nathan-code@nathantreid.com>
2016-04-10 01:02:13 -0500

    version bump to 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'nathantreid:css-modules',
-	version: '1.0.0',
+	version: '1.0.1',
 	summary: 'CSS modules implementation. CSS for components!',
 	git: 'https://github.com/nathantreid/meteor-css-modules.git',
 	documentation: 'README.md'
