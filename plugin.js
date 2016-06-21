0bfa506666b08b06d08718898c7bea1ec1827495
Nathan Reid <nathan-code@nathantreid.com>
2016-06-21 00:40:33 -0500

    enable compilation of CSS files!

--- plugin.js
+++ plugin.js
@@ -5,10 +5,43 @@ const pluginOptions = pluginOptionsWrapper.options;
 
 ImportPathHelpers.init(Plugin);
 
-Plugin.registerCompiler({
-	extensions: pluginOptions.extensions,
-	archMatching: pluginOptions.specificArchitecture,
-	filenames: pluginOptions.filenames
-}, function () {
-	return new CssModulesBuildPlugin(Plugin);
-});
+if (pluginOptions.extensions.indexOf('css') === -1)
+	registerCompiler();
+else
+	monkeyPatchToHandleCssExtension();
+
+
+function registerCompiler() {
+	Plugin.registerCompiler({
+		extensions: pluginOptions.extensions,
+		archMatching: pluginOptions.specificArchitecture,
+		filenames: pluginOptions.filenames
+	}, function () {
+		return new CssModulesBuildPlugin(Plugin);
+	});
+}
+
+/**
+ * Monkey patch _registerSourceProcessor and SourceProcessorSet.merge so we can block the default CSS compiler
+ */
+function monkeyPatchToHandleCssExtension() {
+	const registerSourceProcessor = Plugin._registerSourceProcessor;
+	Plugin._registerSourceProcessor = function (options, factory, {sourceProcessorSet, methodName, featurePackage}) {
+		const buildPluginMerge = sourceProcessorSet.constructor.prototype.merge;
+		sourceProcessorSet.constructor.prototype.merge = function (otherSet, options) {
+			/**
+			 * If a css plugin handler has already been added,
+			 * don't merge the meteor package, which only includes the 'css' package
+			 */
+			if (otherSet._myPackageDisplayName !== 'meteor' || !('css' in this._byExtension)) {
+				buildPluginMerge.call(this, ...arguments);
+				return;
+			}
+		};
+		registerSourceProcessor(options, factory, {sourceProcessorSet, methodName, featurePackage});
+	};
+
+	registerCompiler();
+
+	Plugin._registerSourceProcessor = registerSourceProcessor;
+}
