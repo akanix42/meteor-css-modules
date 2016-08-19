3e46b5607eb01f8d888c5af4b3b8e30cc14f5ad5
Nathan Reid <nathan-code@nathantreid.com>
2016-08-19 17:54:39 -0500

    code style update

--- check-npm-package.js
+++ check-npm-package.js
@@ -7,36 +7,38 @@ import semver from 'semver';
 import colors from 'colors';
 import logger from './logger';
 
-export default function checkNpmPackage(packageWithVersion ) {
-	const [ packageName, packageVersion ] = packageWithVersion.split('@');
+export default function checkNpmPackage(packageWithVersion) {
+  const [ packageName, packageVersion ] = packageWithVersion.split('@');
 
-	if (!verifyPackageExists(packageName, packageVersion))
-		return false;
+  if (!verifyPackageExists(packageName, packageVersion)) {
+    return false;
+  }
 
-	return checkNpmVersion(packageName, packageVersion);
+  return checkNpmVersion(packageName, packageVersion);
 }
 
 function verifyPackageExists(packageName, packageVersion) {
-	const packagePath = `${ImportPathHelpers.basePath}/node_modules/${packageName}`;
-	const doesPackageExist = fs.existsSync(packagePath);
-	if (!doesPackageExist)
-		logger.error(colors.red.bold(`Error checking npm module: ${packageName}@${packageVersion} (required by nathantreid:css-modules): module not found. Please ensure you have installed the module; here is the command:\n meteor npm install ${packageName} --save-dev\n`));
+  const packagePath = `${ImportPathHelpers.basePath}/node_modules/${packageName}`;
+  const doesPackageExist = fs.existsSync(packagePath);
+  if (!doesPackageExist) {
+    logger.error(colors.red.bold(`Error checking npm module: ${packageName}@${packageVersion} (required by nathantreid:css-modules): module not found. Please ensure you have installed the module; here is the command:\n meteor npm install ${packageName} --save-dev\n`));
+  }
 
-	return doesPackageExist;
+  return doesPackageExist;
 }
 
-function checkNpmVersion(name, version){
-	try {
-		const installedVersion = require(`${name}/package.json`).version;
-		if (semver.satisfies(installedVersion, version)) {
-			return true;
-		} else {
-			logger.warn(colors.yellow.bold(`WARNING: version mismatch for ${name}; installed version is ${installedVersion}, but version ${version} is required by nathantreid:css-modules)`));
-			return true;
-		}
-	} catch (e) {
-		logger.error(colors.red.bold(`Error checking package: ${name}@${version} (required by nathantreid:css-modules): ${e.message}`));
-		return false;
-	}
+function checkNpmVersion(name, version) {
+  try {
+    const installedVersion = require(`${name}/package.json`).version;
+    if (semver.satisfies(installedVersion, version)) {
+      return true;
+    } else {
+      logger.warn(colors.yellow.bold(`WARNING: version mismatch for ${name}; installed version is ${installedVersion}, but version ${version} is required by nathantreid:css-modules)`));
+      return true;
+    }
+  } catch (e) {
+    logger.error(colors.red.bold(`Error checking package: ${name}@${version} (required by nathantreid:css-modules): ${e.message}`));
+    return false;
+  }
 }
 
