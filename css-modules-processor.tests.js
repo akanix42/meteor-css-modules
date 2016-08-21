eb5525a333da92f94cf02d83affbed5ebbe29dd2
Nathan Reid <nathan-code@nathantreid.com>
2016-08-20 21:32:29 -0500

    tests for CssModulesProcessor JS class names exports

--- css-modules-processor.tests.js
+++ css-modules-processor.tests.js
@@ -30,12 +30,39 @@ describe('CssModulesProcessor', function() {
       expect(file.contents).to.equal('._test__test { color: red; } ._test__test2 { color: blue; }\n/*# sourceMappingURL=test.css.map */');
     });
 
-    it('should export the class names as JSON', function z() {
-      throw new Error();
+    it('should export the class names as an object', async function z() {
+      const file = {
+        importPath: './test.css',
+        contents: '.test { color: red; } .test-two { color: blue; }',
+        getPathInPackage() {
+          return './test.css';
+        }
+      };
+      const processor = new CssModulesProcessor(pluginOptions);
+      await processor.process(file);
+
+      expect(file.tokens).to.eql({
+        'test': '_test__test',
+        'test-two': '_test__test-two'
+      });
     });
 
-    it('should camelcase the JSON class names when the camelcase option is enabled', function z() {
-      throw new Error();
+    it('should camelcase the JSON class names when the camelcase option is enabled', async function z() {
+      const file = {
+        importPath: './test.css',
+        contents: '.test { color: red; } .test-two { color: blue; }',
+        getPathInPackage() {
+          return './test.css';
+        }
+      };
+      pluginOptions.jsClassNamingConvention.camelCase = true;
+      const processor = new CssModulesProcessor(pluginOptions);
+      await processor.process(file);
+
+      expect(file.tokens).to.eql({
+        'test': '_test__test',
+        'testTwo': '_test__test-two'
+      });
     });
   });
 });
