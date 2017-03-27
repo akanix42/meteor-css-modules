9b736991031345ddaafba21b02ccc579f604a736
Nathan Reid <nathan-code@nathantreid.com>
2017-03-27 07:16:29 -0500

    Support node-sass v4.x

--- options.js
+++ options.js
@@ -94,7 +94,7 @@ function checkSassCompilation(options) {
 
   if (options.enableSassCompilation === true ||
     (Array.isArray(options.enableSassCompilation) && R.intersection(options.enableSassCompilation, options.extensions).length)) {
-    const result = checkNpmPackage('node-sass@3.x');
+    const result = checkNpmPackage('node-sass@>=3.x <=4.x');
     if (result === true) return;
   }
   options.enableSassCompilation = false;
