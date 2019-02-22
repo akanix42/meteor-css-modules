e5ae8c04e3fc095509d568ae8daabe4668cede6f
Nathan Reid <nathan-code@nathantreid.com>
2019-02-22 17:46:44 -0600

    update linux prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
 rm -rf /s /q ./meteor-css-modules
 rsync -d --exclude-from prepare-exclude.rsync.list --exclude='*/' ./ ./meteor-css-modules
 rsync -d --exclude-from prepare-exclude.rsync.list --exclude='*/' ./helpers/ ./meteor-css-modules/helpers
+rsync -d --exclude-from prepare-exclude.rsync.list --exclude='*/' ./package/ ./meteor-css-modules/package
